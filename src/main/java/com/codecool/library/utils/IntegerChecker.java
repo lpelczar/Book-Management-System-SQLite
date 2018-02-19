917bf08dc1bc03cf8a2982cb49b0ae03c75d5e96
lpelczar <lukepelczar@gmail.com>
2018-02-19 10:33:17 +0100

    Add integer checker class

---
+++ src/main/java/com/codecool/library/utils/IntegerChecker.java
@@ -0,0 +1,13 @@
+package com.codecool.library.utils;
+
+public class IntegerChecker {
+
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch(NumberFormatException | NullPointerException e) {
+            return false;
+        }
+        return true;
+    }
+}
