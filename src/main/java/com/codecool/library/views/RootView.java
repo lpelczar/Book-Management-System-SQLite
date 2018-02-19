4109bb0081472117d1aeeb84fe815be242ee97b9
lpelczar <lukepelczar@gmail.com>
2018-02-19 10:27:56 +0100

    Add root view class with displaying menu option

---
+++ src/main/java/com/codecool/library/views/RootView.java
@@ -0,0 +1,21 @@
+package com.codecool.library.views;
+
+public class RootView extends AbstractView {
+
+    public void displayMenu() {
+
+        clearConsole();
+        System.out.println("\nWhat do you want to do?\n" +
+                " 1. Add new book\n" +
+                " 2. Edit book\n" +
+                " 3. Delete book\n" +
+                " 4. Search for book\n" +
+                " 5. Show all books sorted by name ascending\n" +
+                " 6. Show all books by given author\n" +
+                " 0. Exit");
+    }
+
+    public void displayWrongInputMessage() {
+        System.out.println("Wrong input!");
+    }
+}
