fa514fbb43062ad4bd05a0091ed2d94aff42e6ce
lpelczar <lukepelczar@gmail.com>
2018-02-19 11:22:33 +0100

    Create book entry contract

---
+++ src/main/java/com/codecool/library/data/contracts/BookEntry.java
@@ -0,0 +1,12 @@
+package com.codecool.library.data.contracts;
+
+public class BookEntry {
+    public static final String TABLE_NAME = "books";
+    public static final String ISBN = "ISBN";
+    public static final String author = "author";
+    public static final String title = "title";
+    public static final String publisher = "publisher";
+    public static final String publication_year = "publication_year";
+    public static final String price = "price";
+    public static final String type = "type";
+}
