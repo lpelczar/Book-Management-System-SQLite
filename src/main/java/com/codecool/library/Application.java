6097993a2b676b7dd9cd70bdd34055d92f3ed40f
lpelczar <lukepelczar@gmail.com>
2018-02-19 10:28:22 +0100

    Start root controller in application class

--- src/main/java/com/codecool/library/Application.java
+++ src/main/java/com/codecool/library/Application.java
@@ -1,6 +1,12 @@
 package com.codecool.library;
 
+import com.codecool.library.controllers.RootController;
+
 public class Application {
 
+    public static void main(String... args) {
 
+        RootController rootController = new RootController();
+        rootController.start();
+    }
 }
