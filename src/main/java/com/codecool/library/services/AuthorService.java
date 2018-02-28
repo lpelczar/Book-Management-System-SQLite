f686afba6aac8b300b45ff5a749289ac18749d47
lpelczar <lukepelczar@gmail.com>
2018-02-28 17:45:15 +0100

    Add author service

---
+++ src/main/java/com/codecool/library/services/AuthorService.java
@@ -0,0 +1,27 @@
+package com.codecool.library.services;
+
+import com.codecool.library.dao.AuthorDAO;
+import com.codecool.library.models.Author;
+import com.codecool.library.views.AuthorView;
+
+import java.util.ArrayList;
+
+public class AuthorService {
+
+    private AuthorDAO authorDAO;
+    private AuthorView authorView;
+
+    public AuthorService(AuthorDAO authorDAO, AuthorView authorView) {
+        this.authorDAO = authorDAO;
+        this.authorView = authorView;
+    }
+
+    public Author getAuthor() {
+        authorView.displayEntriesNoInput(new ArrayList<>(authorDAO.getAll()));
+        int id;
+        do {
+            id = authorView.getAuthorIdInput();
+        } while (authorDAO.getById(id) == null);
+        return authorDAO.getById(id);
+    }
+}
