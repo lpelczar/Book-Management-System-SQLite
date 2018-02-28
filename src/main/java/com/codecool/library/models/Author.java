c19b93a612b79877954700c00074fdb8bff9ecdd
lpelczar <lukepelczar@gmail.com>
2018-02-28 15:20:40 +0100

    Add Author model

---
+++ src/main/java/com/codecool/library/models/Author.java
@@ -0,0 +1,68 @@
+package com.codecool.library.models;
+
+public class Author {
+
+    private int id;
+    private String name;
+    private String surname;
+    private int birth_year;
+    private String city;
+    private String country;
+
+    public Author(int id, String name, String surname, int birth_year, String city, String country) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.birth_year = birth_year;
+        this.city = city;
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getBirth_year() {
+        return birth_year;
+    }
+
+    public void setBirth_year(int birth_year) {
+        this.birth_year = birth_year;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
