5d19089e3e0369c6f778c952d4c6b3cc5ecb9195
lpelczar <lukepelczar@gmail.com>
2018-02-28 18:41:12 +0100

    Modify application class to reflect changes in constructors

--- src/main/java/com/codecool/library/Application.java
+++ src/main/java/com/codecool/library/Application.java
@@ -1,17 +1,34 @@
 package com.codecool.library;
 
 import com.codecool.library.controllers.RootController;
-import com.codecool.library.dao.DbBookDAO;
+import com.codecool.library.dao.*;
+import com.codecool.library.services.AuthorService;
 import com.codecool.library.services.BookService;
-import com.codecool.library.views.BookView;
-import com.codecool.library.views.RootView;
+import com.codecool.library.services.BookTypeService;
+import com.codecool.library.services.PublisherService;
+import com.codecool.library.views.*;
 
 public class Application {
 
     public static void main(String... args) {
 
-        BookService bookService = new BookService(new DbBookDAO(), new BookView());
-        RootController rootController = new RootController(new RootView(), bookService);
+        AuthorDAO authorDAO = new DbAuthorDAO();
+        PublisherDAO publisherDAO = new DbPublisherDAO();
+        BookTypeDAO bookTypeDAO = new DbBookTypeDAO();
+        BookDAO bookDAO = new DbBookDAO(authorDAO, publisherDAO, bookTypeDAO);
+
+        BookView bookView = new BookView();
+        AuthorView authorView = new AuthorView();
+        PublisherView publisherView = new PublisherView();
+        BookTypeView bookTypeView = new BookTypeView();
+        RootView rootView = new RootView();
+
+        AuthorService authorService = new AuthorService(authorDAO, authorView);
+        PublisherService publisherService = new PublisherService(publisherDAO, publisherView);
+        BookTypeService bookTypeService = new BookTypeService(bookTypeDAO, bookTypeView);
+        BookService bookService = new BookService(bookDAO, bookView, authorService, publisherService, bookTypeService);
+
+        RootController rootController = new RootController(rootView, bookService);
         rootController.start();
     }
 }
